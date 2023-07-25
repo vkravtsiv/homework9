e6e167512c00bed505e0ae200766584fde45d63f
Vasyl Kravtsiv <vkravtsiv@gmail.com>
2023-07-25 03:26:55 +0300

    third commit;

--- src/MyStack.java
+++ src/MyStack.java
@@ -7,80 +7,118 @@ public class MyStack<T> {
 
 
     private int tos;
+    private int length;
+    private int size=0;
+    MyStack() {
 
-    MyStack(int size) {
-        stck = (T[]) new Object[size];
+        stck = (T[]) new Object[length];
         tos = -1;
+    }
+
+    MyStack(int length) {
+        this();
+        this.length=length;
+
     }
 
     void push(T item) {
         if (tos == stck.length - 1) {
-            T[] temp = (T[])new Object[stck.length * 2];
-            for (int i = 0; i < stck.length; i++) {
+            length=length*2+1;
+            T[] temp = (T[])new Object[length];
+            for (int i = 0; i <stck.length; i++) {
                 temp[i] = stck[i];
             }
             stck = temp;
         }
+
         stck[++tos] = item;
+        size++;
     }
 
     boolean isEmpty(){
         return tos==-1;
     }
-    Object peek(){
+    T peek(){
         if (!isEmpty())
             return stck[tos];
 
         return null;
     }
-    Object pop() {
+    T pop() {
+
         if (isEmpty()) {
             System.out.println("Stack is empty");
             return null;
 
-        } else
-            return stck[tos--];
+        } else {
+            T result=stck[tos--];
+            stck[++tos]=null;
+            tos--;
+            size--;
+
+            return result;
+        }
+
 
     }
-    Object remove(int index){
+    T remove(int index){
+        T result=stck[index];
         for (int i = index; i < stck.length-1 ; i++) {
            stck[i]= stck[i+1];
         }
         stck[stck.length-1]=null;
-        return stck;
+        tos--;
+        size--;
+        return result;
 
     }
     int size(){
-        int result=0;
-        for (int i = 0; i < stck.length; i++) {
-            if (stck[i]!= null) result++;
-        }
-        return result;
+//        int result=0;
+//        for (int i = 0; i < stck.length; i++) {
+//            if (stck[i]!= null) result++;
+//        }
+//        return result;
+        return size;
     }
     int clear(){
-        for (int i = 0; i < stck.length; i++) {
-            stck[i] = null;
-        }
+        length=0;
+        stck = (T[]) new Object[length];
+        size=0;
+        tos=-1;
         return 0;
     }
+
+
+    @Override
+    public String toString() {
+        T[] stckOut=(T[]) new Object[size];
+        for (int i = 0; i <size ; i++) {
+            stckOut[i]=stck[i];
+        }
+        return "MyStack{" +
+                "stck=" + Arrays.toString(stckOut) +
+                ", size=" + size +
+                '}';
+    }
+
     public static void main(String[] args) {
-        MyStack<String> stack1=new MyStack(5);
+        MyStack<String> stack1=new MyStack();
+        stack1.push("5");
         stack1.push("50");
         stack1.push("500");
-        stack1.push("true");
         stack1.push("5000");
         stack1.push("50000");
-        stack1.push("5");
+        stack1.push("500000");
 
-            System.out.println(Arrays.toString(stack1.stck));
+            System.out.println(stack1);
 
 
         System.out.println(stack1.pop());
         System.out.println(stack1.pop());
 //        System.out.println(stack1.pop());
 //        System.out.println(stack1.pop());
-        stack1.remove(1);
-        System.out.println(Arrays.toString(stack1.stck));
+        System.out.println(stack1.remove(1));
+        System.out.println(stack1);
         System.out.println("tos = " + stack1.tos);
         System.out.println("stack1.peek() = " + stack1.peek());
         System.out.println("stack1.size() = " + stack1.size());
